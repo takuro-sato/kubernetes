c8e82997a1d6ccebb64b083f4756215ab8c35d95
Takuro Sato <takurosato@microsoft.com>
2023-07-12 13:20:32 +0000

    Use seed=17

--- test/conformance/image/go-runner/cmd.go
+++ test/conformance/image/go-runner/cmd.go
@@ -42,7 +42,7 @@ func getCmd(env Getenver, w io.Writer) *exec.Cmd {
 	ginkgoArgs = append(ginkgoArgs, []string{
 		"--focus=" + env.Getenv(focusEnvKey),
 		"--skip=" + skip,
-		"--noColor=true", " --seed=0",
+		"--noColor=true", "--seed=17",
 	}...)
 
 	extraArgs := []string{
